c6ba7a246cf1cf4e67d4dd9e8c3b3cf1874da47b
Belin27000 <y.lecerf@protonmail.com>
2023-09-10 19:33:26 +0200

    feat:home page done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import Smiley from '@/assets/smiley.jpg'
+import conflict from '@/assets/images/Conflits.png'
+import despress from '@/assets/images/Dépression.png'
+import Phobies from '@/assets/images/Phobies.png'
+import Disease from '@/assets/images/Maladie.png'
 import '@/pages/home.css'
+import { useTypingeffect } from '../hooks/typing-effect.js';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+
+    const text = useTypingeffect(" \"La folie c'est de faire tout le temps la même chose et de s'attendre à un résultat différent\" A.Einstein", 125);
     return (
         <div className='Home'>
-            <div>
+            <section className='HomeIntro'>
                 <blockquote className='quote'>
-                    <p>"La folie c'est de faire tout le temps la même chose
-                        et de s'attendre à un résultat différent" A.Einstein</p>
+                    <p>{text}</p>
                 </blockquote>
                 <p>Cette citation résume à elle seule très bien la thérapie brève systémique et stratégique !
                 </p>
@@ -19,21 +25,55 @@ const Home = () => {
                 <p>
                     Cette approche conduit à un <span className='textHightLight'>changement durable</span> par une intervention brève allant de <span className='textHightLight'>1 à 10 séances.</span> Il ne s'agit pas ici d'analyser pourquoi le problème est apparu ou bien où il trouve ses origines mais de comprendre <span className='textHightLight'>comment il fonctionne</span> et surtout <span className='textHightLight'>comment il se maintient.</span>
                 </p>
-                <section>
-                    <div className='difficultyList'>
-                        <ul>
-                            <li>Conflits relationnels personnels ou professionnels</li>
-                            <li>Anxiété</li>
-                            <li>Dépression</li>
-                            <li>Peur & phobies</li>
-                            <li>...</li>
-                        </ul>
-                        <p>La thérapie brève peut vous aider à surmonter ces difficultés.</p>
+            </section>
+            <section className="difficulty">
+                <div className="difficultyHead">
+                    <p>Vous rencontrez une difficulté de vie ou un problème ? Vous avez tout essayer ? Vous vous sentez coincé dans une situation, épuisé et avez l’impression que rien ne pourra changer ?</p>
+                </div>
+                <div className="damierGlobal">
+                    <div className="damierLeft">
+
+                        <div className="conflict">
+                            <div className="conflictPic">
+                                <img src={conflict} alt='conflits relationnels' />
+                            </div>
+                            <div className="conflictDes">
+                                <h3 className='TitleDifficulties'>CONFLITS RELATIONNELS PROFESSIONNELS OU PERSONNELS</h3>
+                            </div>
+                        </div>
+                        <div className="despress">
+                            <div className="despressDes">
+                                <h3 className='TitleDifficulties'>DÉPRESSION, STRESS, ANXIÉTÉ</h3>
+                            </div>
+                            <div className="despresstPic">
+                                <img src={despress} alt='depression' />
+                            </div>
+                        </div>
                     </div>
-                    <div className='imgContainer'>
-                        <img className='Smiley' src={Smiley} alt='smiley' />
+                    <div className="damierRight">
+
+                        <div className="Phobies">
+                            <div className="PhobiesPic">
+                                <img src={Phobies} alt='peur et phobies' />
+                            </div>
+                            <div className="PhobiesDes">
+                                <h3 className='TitleDifficulties'>PEURS ET PHOBIES</h3>
+                            </div>
+                        </div>
+                        <div className="Disease">
+                            <div className="DiseaseDes">
+                                <h3 className='TitleDifficulties'>MALADIES CHRONIQUES</h3>
+                            </div>
+                            <div className="DiseasePic">
+                                <img src={Disease} alt='maladie chronique' />
+                            </div>
+                        </div>
                     </div>
-                </section>
+                </div>
+            </section>
+            <div className="more">
+                <p>La thérapie brève peut vous aider à surmonter ces difficultés.</p>
+                <Link to="/accompagnement">Plus d'informations</Link>
             </div>
         </div>
     );
