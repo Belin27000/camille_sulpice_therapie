7bbdd1c72c91ead5019bf05f1527f24b13201ab1
Belin27000 <y.lecerf@protonmail.com>
2023-09-15 13:24:29 +0200

    images optimization

--- src/components/public/Header.js
+++ src/components/public/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import CSulpice_logo from '@/assets/Logo_CS.webp'
-import Title from '@/assets/Title_CS.webp'
+import Title from '@/assets/images/Title_CS_wcarza_c_scale,w_500.webp'
+import TitleBig from '@/assets/images/Title_CS_wcarza_c_scale,w_1584.webp'
 import '@/components/public/header.scss'
 import NavBar from '@/components/NavBar/NavBar.js';
 
 const Header = () => {
     return (
+
         <header className='Header'>
             <div>
                 <img className='Logo' src={CSulpice_logo} alt='logo Camille Sulpice' />
-                <img className='TitleHeader' src={Title} alt='logo thérapie brève stratégique et systémique' />
+                <img className='TitleHeader' src={TitleBig} srcSet={`${Title} 800w,${TitleBig} 1000w`} sizes="(max-width: 1584px) 100vw, 1584px" alt="logo thérapie brève stratégique et systémique" />
             </div>
             <NavBar />
 
